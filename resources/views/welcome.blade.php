6d6dbfd1b1a51d8f27a3edd4c6455d2d215fc356
Gazi HATAŞ <gazihatas71@gmail.com>
2022-03-05 07:19:54 +0300

    pagination işlemleri tamamlandı

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -45,15 +45,15 @@
                   <div class="container">
                     <h1>Album example</h1>
                     <p class="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-                   
+
                   </div>
                 </section>
-              
+
                 <div class="album py-5 bg-light">
                   <div class="container">
-              
+
                     <div class="row">
-            
+
                       @foreach ($posts as $post)
                       <div class="col-md-4">
                         <div class="card mb-4 shadow-sm">
@@ -64,7 +64,7 @@
                             <div class="d-flex justify-content-between align-items-center">
                               <div class="btn-group">
                                 <a href="{{ route('post.show', $post->slug) }}" class="btn btn-sm btn-outline-secondary">View</a>
-                                
+
                               </div>
                               <small class="text-muted">{{ $post->user->name }}</small>
                               <small class="text-muted">{{ $post->updated_at }}</small>
@@ -72,16 +72,16 @@
                           </div>
                         </div>
                       </div>
-                     
+
                       @endforeach
-              
-                      {{ $posts->links() }}
+
+                      {{ $posts->links('pagination::bootstrap-4') }}
                     </div>
                   </div>
                 </div>
-              
+
               </main>
-            
+
         </div>
 
 
